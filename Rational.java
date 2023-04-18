b1d155e1e3ff19a35e02b873ffd04255ac416e49
Escobachar <bacyuv@bgu.ac.il>
2023-04-18 19:48:17 +0300

    IntegerScalar and Monomial implementations

--- Rational.java
+++ Rational.java
@@ -3,26 +3,18 @@ public class Rational implements Scalar {
     private int denominator;
 
     public Rational(Double value){
-       String strValue = value.toString();
-       int digitsDec = strValue.length() - 1 - strValue.indexOf('.');        
-
-
-       int denominator = 1;
-       for(int i = 0; i < digitsDec; i++){
-           value *= 10;
-           denominator *= 10;
-       }
-       int numerator = (int) Math.round(value);
-
-       this.numerator = numerator;
-       this.denominator = denominator;
-    }
-    public Rational(int numerator, int denominator){
-       if(denominator == 0){
-              throw new IllegalArgumentException("The denomitor can't be 0!");
-       }
-       this.numerator = numerator;
-       this.denominator = denominator;
+        int numerator;
+        int denominator;
+        String strValue = value.toString();
+        int pointLoc = strValue.indexOf(".");
+        numerator = Integer.parseInt(strValue.substring(0, pointLoc));
+        denominator = Integer.parseInt(strValue.substring(pointLoc+1));
+        for(int i = pointLoc; i< strValue.length(); i++){
+            numerator*=10;
+            denominator*=10;
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
 	@Override
@@ -34,33 +26,27 @@ public class Rational implements Scalar {
 
 	@Override
 	public Scalar mul(Scalar s) {
-              double result = this.getValue() * s.getValue();
-              return new Rational(result);
+       return null;
 	}
 
 	@Override
 	public Scalar neg() {
-              int newNumerator = this.numerator *= -1;
-              return new Rational(newNumerator, this.denominator);
+       return null;
 	}
 
 	@Override
 	public Scalar power(int exponent) {
-              int newNumerator = (int)Math.pow(numerator, exponent);
-              int newDenomerator = (int)Math.pow(denominator, exponent);
-              return new Rational(newNumerator, newDenomerator);
+       return null;
 	}
 
 	@Override
 	public int sign() {
-              if((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))
-                     return -1;
-              return 1;
+       return -1;
 	}
 
 	@Override
 	public double getValue() {
-       return (this.numerator/(double)this.denominator);
+       return (this.numerator/this.denominator);
 	}
     
 }
