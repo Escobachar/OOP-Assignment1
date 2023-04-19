01b199dcece478e8c699f789456316acc9750d2d
Ron Yaacov <ronyaacov19@gamil.com>
2023-04-19 08:59:14 +0300

    build class polynomial

---
+++ Polynomial.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+public class Polynomial {
+    private LinkedList<Monomial> monomials;
+    
+    public static Polynomial build(String input){
+        return null;
+    }
+    
+    public Polynomial add(Polynomial p){
+        return null;
+    }
+
+    public Polynomial mul(Polynomial p){
+        return null;
+    }
+
+    public Scalar evaluate(Scalar s){
+        return null;
+    } 
+    public Polynomial derivative(){
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return false;
+    }
+    @Override
+    public String toString(){
+        return null;
+    }
+
+    
+}
